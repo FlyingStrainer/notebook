447d4d7666241b3838eca0c8e6e2a53b81e770c7
patel627 <patel627@purdue.edu>
2017-10-05 23:42:46 -0400

    updates to email.js

--- app/email.js
+++ app/email.js
@@ -1,38 +1,23 @@
-var firebase = require('firebase-admin')
-
-var config = {
-    apiKey: "AIzaSyBuBYxnmn16RZYxJs-X_xOBbbft2VIkBPg",
-    authDomain: "vent-91586.firebaseapp.com",
-    databaseURL: "https://vent-91586.firebaseio.com",
-    projectId: "vent-91586",
-    storageBucket: "vent-91586.appspot.com",
-    messagingSenderId: "789865238756"
-  };
-firebase.initializeApp(config);
-
-var database = firebase.database();
-
 var nodemailer = require('nodemailer');
-
+var emailAddTo = "mehulap8@gmail.com";
+var newUserName = "John Doe";
 var transport = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'supportvennote@gmail.com', //just used a personal gmail account for testing
-    pass: 'pass'
+    user: 'VENoteApp@gmail.com', //just used a personal gmail account for testing
+    pass: 'VENote2017'
   }
 });
 
-function getData(user) {
   var mailOptions = {
-    from: 'support@vennote.com',
-    to: 'company@company.net',
+    from: '',
+    to: emailAddTo,
     subject: 'New User Signed up to view Notebook...',
-    text: 'Hi,' + dataEl.username + ' has signed up. If you did not approve this, please....'
+    text: 'Hi,' + newUserName + ' has signed up. If you did not approve this, please....'
   };
-  return mailOptions;
-}
 
-transport.sendMail(getDate(user), function(error, info){
+
+transport.sendMail(mailOptions, function(error, info){
   if (error) {
     console.log(error);
   } else {
