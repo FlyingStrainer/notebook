e6e454eb1c7ee89a865ff444d8224277b63114a2
jarett-lee <jarett.lee.pi@gmail.com>
2017-10-18 17:41:55 -0400

    Add pdf exist test

--- app/src/PDFGen.js
+++ app/src/PDFGen.js
@@ -1,5 +1,5 @@
-PDFDocument = require ("pdfkit");
-fs = require("fs");
+const PDFDocument = require ("pdfkit");
+const fs = require("fs");
 
 
 
@@ -7,29 +7,29 @@ fs = require("fs");
 
 module.exports = {
     genPDF : function (t1, t2, i1, i2, pdfName) {
-        doc = new PDFDocument();
-        
+        const doc = new PDFDocument();
+
         // Pipe its output somewhere, like to a file or HTTP response
         // See below for browser usage
         doc.pipe(fs.createWriteStream(pdfName + '.pdf'));
-        
+
         // Embed a font, set the font size, and render some text
         //doc.font('fonts/PalatinoBold.ttf')
         //   .fontSize(25)
         //   .text('Some text with an embedded font!', 100, 100)
-        
+
         // Add another page
         doc.fontSize(12);
         doc.text(t1, 100, 200)
         doc.image(i1,100, 500, {width: 300});
-        
-        
-        
+
+
+
         // Add some text with annotations
         doc.addPage()
         .text(t2, 100, 400)
         .image(i2,300, 500, {width: 300});
-        
+
         // Finalize PDF file
         doc.end()
     }
---
+++ app/src/PDFGen.test.js
@@ -0,0 +1,11 @@
+
+const PDFGen = require('./PDFGen');
+const fs = require('fs');
+
+it('tests', () => {
+  const fname = 'pdfgenout.test';
+  PDFGen.genPDF('test', 'test,', 'testimage.jpg', 'testimage.jpg', fname);
+  expect(() => {
+    fs.unlinkSync(fname + '.pdf');
+  }).not.toThrow();
+});
