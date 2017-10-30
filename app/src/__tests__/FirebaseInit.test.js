69b0aac6ad865e1fb5f99a39bcf218c4a08af90f
jarett-lee <jarett.lee.pi@gmail.com>
2017-10-30 19:11:21 -0400

    Add #addEntry test

--- app/src/__tests__/FirebaseInit.test.js
+++ app/src/__tests__/FirebaseInit.test.js
@@ -1,20 +1,32 @@
 const firebase = require('../FirebaseInit.js');
 
+const testUserData = {
+  user_hash: 'testUserHash',
+  notebooks: [
+    {
+      uid: 'notebookHash1',
+      name: 'notebookName1',
+      author: 'Test User',
+      data_entries: [],
+      tags: expect.any(Array),
+    },
+  ],
+};
+
+const testDataEntry = {
+  uuid: 'testUuid',
+  text: '',
+  // image: '',
+  // caption: '',
+  date_created: '',
+  author_id: '',
+  tags: {
+    tag_string: '',
+  },
+};
+
 
 test('#getNotebooks', (done) => {
-  const testUserData = {
-    user_hash: 'testUserHash',
-    notebooks: [
-      {
-        uid: 'notebookHash1',
-        name: 'notebookName1',
-        author: 'Test User',
-        data_entries: [],
-        tags: expect.any(Array),
-      },
-    ],
-  };
-
   expect.assertions(2);
 
   firebase.getNotebooks(testUserData.user_hash).then((data) => {
@@ -27,7 +39,14 @@ test('#getNotebooks', (done) => {
 // TODO clean up new notebook
 test.skip('#saveNotebook', (done) => {
   const notebookName = 'name';
-  firebase.saveNotebook(pName).then(() => {
+  firebase.saveNotebook(notebookName).then(() => {
+    done();
+  });
+});
+
+// TODO clean up added entry
+test.skip('#addEntry', (done) => {
+  firebase.addEntry(testDataEntry).then(() => {
     done();
   });
 });
