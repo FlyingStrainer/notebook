618396fdf35e7fbf485cecb769a1a40fb7855e1a
jarett-lee <jarett.lee.pi@gmail.com>
2017-10-30 18:58:52 -0400

    Add FirebaseInit#getNotebooks test

---
+++ app/src/__tests__/FirebaseInit.test.js
@@ -0,0 +1,32 @@
+const firebase = require('../FirebaseInit.js');
+
+
+test('#getNotebooks', (done) => {
+  const testUserData = {
+    user_hash: 'testUserHash',
+    notebooks: [
+      {
+        uid: 'notebookHash1',
+        name: 'notebookName1',
+        author: 'Test User',
+        data_entries: [],
+        tags: expect.any(Array),
+      },
+    ],
+  };
+
+  expect.assertions(2);
+
+  firebase.getNotebooks(testUserData.user_hash).then((data) => {
+    expect(data).toBeDefined();
+    expect(data).toEqual(testUserData);
+    done();
+  });
+});
+
+// test('#saveNotebook', (done) => {
+//   const pName = 'cool';
+//   firebase.saveNotebook(pName).then(() => {
+//     done();
+//   });
+// });
