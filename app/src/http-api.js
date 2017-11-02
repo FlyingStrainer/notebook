62fa6f7d4caa5a86c540d08aa2fba3e0b7795f7d
Jarett Lee <jarett.lee.pi@gmail.com>
2017-11-01 22:35:43 -0400

    Commit

--- app/src/http-api.js
+++ app/src/http-api.js
@@ -6,6 +6,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const firebase = require('./firebase-util.js');
+const Notebook = require('./objects/Notebook');
 
 // Setup
 // const db = admin.database();
@@ -16,13 +17,27 @@ router.use(require('access-control')({
 }));
 
 // Define the port to run on
-router.set('port', 3000);
+router.set('port', 80);
 
 // Middleware
 router.use(bodyParser.json());
 
 // API
 // TODO api is a mess
+
+router.post('/user', (req, res) => {
+  console.log('req');
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({
+    user_hash: 'user_hash1',
+    company_name: 'company1',
+    notebooks: [
+      'notebookhash1',
+      'notebookhash2',
+    ],
+    role: 'user',
+  }), null, 3);
+});
 
 // writing
 router.post('/saveNotebook', (req, res) => {
@@ -67,14 +82,45 @@ router.get('/getEntries', async (req, res) => {
   });// old: await db.ref(`words/${userId}`).once('value');
 });
 
-router.get('/getNotebooks', async (req, res) => {
-  const {user_hash} = req.query;
-  firebase.getNotebooks(user_hash, (snapshot) => {
-    // This is done so that if the user does not exist, a empty obj is returned
-    const response = Object.assign({}, snapshot.val());
+router.post('/getNotebooks', (req, res) => {
+  console.log('req: /getNotebooks');
 
-    res.send(response);
-  });// old: await db.ref(`words/${userId}`).once('value');
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({
+    notebooks: [
+      'notebookhash1',
+      'notebookhash2',
+    ],
+  }));
+});
+
+router.post('/getNotebook', (req, res) => {
+  console.log('req: /getNotebook');
+  const {notebook_hash} = req.body;
+
+  res.setHeader('Content-Type', 'application/json');
+  if (notebook_hash == 'notebookhash1') {
+    res.send(JSON.stringify({
+      uuid: notebook_hash,
+      name: 'notebook name 1',
+      managers: [ 'manager1' ],
+      date_modified: new Date(),
+      date_created: new Date(),
+      tags: [ 'tag1' ],
+      permisions: { 'user': true },
+    }));
+  }
+  else {
+    res.send(JSON.stringify({
+      uuid: notebook_hash,
+      name: 'notebook name 2',
+      managers: [ 'manager2' ],
+      date_modified: new Date(),
+      date_created: new Date(),
+      tags: [ 'tag1', 'tag2' ],
+      permisions: { 'user': true },
+    }));
+  }
 });
 
 module.exports = router;
--- app/src/server.js
+++ app/src/server.js
@@ -1,6 +1,8 @@
 
 const app = require('./http-api');
 
+console.log('trying to start server');
+
 // Listen for requests
 const server = app.listen(app.get('port'), () => {
   const {port} = server.address();
