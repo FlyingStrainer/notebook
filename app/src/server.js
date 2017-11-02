c858c6d022d1efa209a78cda7d28773f31755dad
Aaron Neustedter <aaron97neu@yahoo.com>
2017-11-01 21:35:42 -0400

    Added option to specify port in the 1st argument of server.js

--- app/src/server.js
+++ app/src/server.js
@@ -1,8 +1,14 @@
 
 const app = require('./http-api');
 
+var requestedport = 3000;
+
+if (process.argv.slice(2)){
+  requestedport = process.argv.slice(2);
+}
+
 // Define the port to run on
-app.set('port', 3000);
+app.set('port', requestedport[0]);
 
 // Listen for requests
 const server = app.listen(app.get('port'), () => {
