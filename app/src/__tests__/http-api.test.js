c66b97687e7ba7ae0b2172de623912d7550cd31b
jarett-lee <jarett.lee.pi@gmail.com>
2017-11-01 20:36:04 -0400

    Add POST /saveNotebook test

--- app/src/__tests__/http-api.test.js
+++ app/src/__tests__/http-api.test.js
@@ -4,14 +4,33 @@ const api = require('../http-api');
 // GET /user HTTP/1.1
 // Authorization: Basic dXNlcm5hbWU6cGFzcw==
 
-describe('#getNotebook', () => {
+describe.skip('GET /user', () => {
   it('should return json', async () => {
-    const response = request(api)
+    const response = await request(api)
       .get('/user')
       .auth('username', 'password');
 
     expect(response).toBeDefined();
     expect(response.statusCode).toBe(200);
     expect(response.header['content-type']).toMatch(/json/);
+  });
+});
+
+// Cleanup test notebook
+describe.skip('POST /saveNotebook', () => {
+  it('should save a notebook', async () => {
+    const data = {
+      name: 'notebook1',
+      author: 'user1'
+    };
+
+    const response = await request(api)
+      .post('/saveNotebook')
+      .set('Content-Type', 'application/json')
+      .auth('username', 'password')
+      .send(data);
+
+    expect(response).toBeDefined();
+    expect(response.statusCode).toBe(201);
   });
 });
