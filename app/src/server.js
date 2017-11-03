4558bb924d8e92bd83d8cae7789a5f3e82d3f83f
jarett-lee <jarett.lee.pi@gmail.com>
2017-11-03 15:02:40 -0400

    Add websocket

--- app/src/server.js
+++ app/src/server.js
@@ -1,17 +1,11 @@
 
 const app = require('./http-api');
+const websocket = require('./websocket');
 
-var requestedport = 3000;
+const requestedport = process.env.PORT || 3000;
 
-if (process.argv.slice(2)){
-  requestedport = process.argv.slice(2);
-}
-
-// Define the port to run on
-app.set('port', requestedport[0]);
+console.log(`Server trying port ${requestedport}`);
 
 // Listen for requests
-const server = app.listen(app.get('port'), () => {
-  const {port} = server.address();
-  console.log(`Server started on port ${port}`);
-});
+const server = app.listen(requestedport);
+websocket.attach(app, server);
--- app/src/websocket.js
+++ app/src/websocket.js
@@ -1,36 +1,93 @@
 
-const WebSocketServer = require('ws').Server;
+var WebSocketServer = require("ws").Server;
 
-module.exports = function addWSS(server) {
-  const wss = new WebSocketServer({server});
-  wss.on('connection', (ws) => {
-    // const location = url.parse(req.url, true);
+// 5 sec timeout
 
-    console.info('websocket connection open');
+function attachWs(app, server) {
+  var wss = new WebSocketServer({server: server});
+
+  var connections = [];
+
+  wss.pushToAll = function pushToAll(users, notebook_hash, entry_hash) {
+    users.forEach(function each(target_user_hash) {
+      connections.forEach(function each(pair) {
+        const ws = pair[0];
+        const ws_user_hash = pair[1];
+        if (target_user_hash == ws_user_hash) {
+          if (ws.readyState === 1) {
+            ws.send(JSON.stringify({type:'push',msg:{notebook_hash, entry_hash}}));
+          }
+        }
+      });
+    });
+  };
+
+  // putToAll using firebase
+
+  wss.on("connection", function (ws) {
+    console.log("websocket connection open");
+
+    let login = false;
+    let failed = false;
+
+    setTimeout(() => {
+      if (!login) {
+        failed = true;
+        ws.send(JSON.stringify({type:'failed'}));
+        ws.close();
+      }
+    }, 10 * 1000);
 
     const timestamp = new Date().getTime();
-    const userId = timestamp;
+    const userId = timestamp
 
-    ws.send(JSON.stringify({
-      msgType: 'onOpenConnection',
-      msg: {
-        connectionId: timestamp,
-      },
-    }));
+    ws.on("message", function incoming(message, flags) {
+      var data = JSON.parse(message);
+      console.log('received: ', data);
 
-    ws.on('message', function incoming(data) {
-      const clientMsg = data;
-      console.log('received: %s', clientMsg);
+      if (data.type === 'login') {
+        const user_hash = data.user_hash;
+        if (!(user_hash) || failed) {
+          failed = true;
+          ws.send(JSON.stringify({type:'failed'}));
+        }
+        else {
+          login = true;
+          ws.send(JSON.stringify({type:'login',msg:user_hash}));
+          connections.push([ws, user_hash]);
+        }
+      }
+      if (data.type === 'testpush') {
+        let myhash = undefined;
+        for (var i = 0; i < connections.length; i++) {
+          const pair = connections[i];
+          if (ws == pair[0]) {
+            myhash = pair[1];
+            break;
+          }
+        }
+        wss.pushToAll([myhash, '--user-key-1', '--manager-key-1'],
+          '--notebook-key-1', '--data-entry-key-1');
+      }
 
-      ws.send(JSON.stringify({msg: {connectionId: userId}}));
+      // add connection test to value
     });
 
-    ws.on('close', () => {
+    ws.on('close', function () {
       console.log('websocket connection close');
+      ws.close();
+
+      // for (var i = 0; i < connections.length; i++) {
+      //   const pair = connections[i];
+      //   if (ws == pair[0]) {
+      //     // remove from connections
+      //     break;
+      //   }
+      // }
     });
   });
 
-  console.log('websocket server created');
-
   return wss;
-};
+}
+
+exports.attach = attachWs;
