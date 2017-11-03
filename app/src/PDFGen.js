622f3785961c2ad0dd5142c5ab6803c630adb42a
patel627 <patel627@purdue.eduL>
2017-11-03 17:26:10 -0400

    pdf gen updates

--- app/src/PDFGen.js
+++ app/src/PDFGen.js
@@ -5,33 +5,32 @@ const fs = require('fs');
 // Create a document
 
 module.exports = {
-  genPDF(t1, t2, i1, i2, pdfName) {
+  genPDF(entries, pdfName) {
     const doc = new PDFDocument();
 
     // Pipe its output somewhere, like to a file or HTTP response
     // See below for browser usage
     doc.pipe(fs.createWriteStream(`${pdfName}.pdf`));
-
+    doc.fontSize(12);
     // Embed a font, set the font size, and render some text
     // doc.font('fonts/PalatinoBold.ttf')
     //   .fontSize(25)
     //   .text('Some text with an embedded font!', 100, 100)
-
-    // Add another page
-    doc.fontSize(12);
-    doc.text(t1, 100, 200);
-    doc.image(i1, 100, 500, {width: 300});
-
-
-    // Add some text with annotations
-    doc.addPage()
-      .text(t2, 100, 400)
-      .image(i2, 300, 500, {width: 300});
+    for (var i = 0; i<entries.size(); i++) {
+      doc.text(entries[i].date, 100, 200);
+      doc.text(entries[i].date, 100, 220);
+      doc.image(entries[i].image, 100, 300, {width: 300});
+      doc.text(entries[i].date, 100, 800);
+      doc.addPage();
+    }
 
     // Finalize PDF file
     doc.end();
   },
 };
-
-// module.exports.genPDF(
-//   "test", "test,", "testimage.jpg", "testimage.jpg", "pdfgenout");
+///date -> text -> image -> caption
+var entries = [{date:"11-2-2017", text:"text1",
+ image: "testimage.jpg", caption:"cap cap cap"}, {date:"11-3-2017", text:"text2",
+  image: "testimage2.jpg", caption:"cap cap cap"}];
+ module.exports.genPDF(
+  "test", "test,", "testimage.jpg", "testimage.jpg", "pdfgenout");
