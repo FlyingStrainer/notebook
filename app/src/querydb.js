164abf8e6440e2dd3b8067c5a00bed07cb96b7f5
patel627 <patel627@purdue.eduL>
2017-11-27 17:26:09 -0500

    fixing querying -- returns results in correct format now

--- app/src/querydb.js
+++ app/src/querydb.js
@@ -1,60 +1,69 @@
-const algoliasearch = require('algoliasearch');
-const dotenv = require('dotenv');
-const firebase = require('firebase');
+var dotenv = require('dotenv');
+var firebaseAdmin = require("firebase-admin");
+var algoliasearch = require('algoliasearch');
 
 // load values from the .env file in this directory into process.env
 dotenv.load();
 
-// configure firebase
-firebase.initializeApp({
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
+var serviceAccount = require("../serviceAccountKey.json");
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
 });
-const database = firebase.database();
+var database = firebaseAdmin.database();
 
-// configure algolia
-const algolia = algoliasearch(
-  process.env.ALGOLIA_APP_ID,
-  process.env.ALGOLIA_API_KEY,
-);
-const index = algolia.initIndex(process.env.ALGOLIA_INDEX_NAME);
+var algolia = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY);
+var index = algolia.initIndex('entries');
 
-database.ref('/Notebooks').once('value', (contacts) => {
-  // Build an array of all records to push to Algolia
-  const records = [];
-  contacts.forEach((contact) => {
+var notebooksRef = database.ref("/Notebooks");
+//clear before import
+index.clearIndex(function(err, content) {
+  console.log("Cleared");
+});
+notebooksRef.once('value', initialImport);
+
+searchForText("test");
+
+function initialImport(dataSnapshot) {
+  
+  // Array of data to index
+  var objectsToIndex = [];
+  // Get all objects
+  var values = dataSnapshot.val();
+  // Process each child Firebase object
+  dataSnapshot.forEach((function(childSnapshot) {
     // get the key and data from the snapshot
-    const childKey = contact.key;
-    const childData = contact.val();
-    // We set the Algolia objectID as the Firebase .key
+    var childKey = childSnapshot.key;
+    var childData = childSnapshot.val();
+    // Specify Algolia's objectID using the Firebase object key
     childData.objectID = childKey;
     // Add object for indexing
-    records.push(childData);
-  });
-
+    objectsToIndex.push(childData);
+  }))
   // Add or update new objects
-  index
-    .saveObjects(records)
-    .then(() => {
-      console.log('Notebooks imported into Algolia');
-    })
-    .catch((error) => {
-      console.error('Error when importing contact into Algolia', error);
-      process.exit(1);
-    });
-});
+  index.saveObjects(objectsToIndex, function(err, content) {
+    if (err) {
+      throw err;
+    }
+    console.log('Firebase<>Algolia import done');
+    process.exit(0);
+  });
+}
 
 
-/* // Search query
-  const query = "test";
-
-  // Perform an Algolia search:
-  // https://www.algolia.com/doc/api-reference/api-methods/search/
-  index
-    .search({
-      query
-    })
-    .then(responses => {
-      // Response from Algolia:
-      // https://www.algolia.com/doc/api-reference/api-methods/search/#response-format
-      console.log(responses.hits);
-    }); */
+function searchForText(text) {
+  // Search query
+  const query = text;
+  
+    // Perform an Algolia search:
+    // https://www.algolia.com/doc/api-reference/api-methods/search/
+    index
+      .search({
+        query
+      })
+      .then(responses => {
+        // Response from Algolia:
+        // https://www.algolia.com/doc/api-reference/api-methods/search/#response-format
+        console.log(responses.hits);
+      }); 
+}
