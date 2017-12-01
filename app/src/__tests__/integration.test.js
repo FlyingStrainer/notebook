93d567660de6e12e730fd92b2dd31f89ca6cf1dd
jarett-lee <jarett.lee.pi@gmail.com>
2017-12-01 18:36:38 -0500

    Add notebook link 404

--- app/src/__tests__/integration.test.js
+++ app/src/__tests__/integration.test.js
@@ -5,46 +5,52 @@ const api = require('../http-api');
 
 const {firebaseAdmin} = firebaseUtil;
 
-// beforeAll(() => {
-//   const updates = {};
-//   updates['/NotebookList/--notebook-hash-1'] = null;
-//   updates['/UserList/--user-hash-1'] = null;
-//   updates['/companies/--company_name'] = null;
-//
-//   return firebaseAdmin.database().ref().update();
-// });
-//
-// async function testApi(path, req, res) {
-//   let response;
-//
-//   try {
-//     response = await request(api)
-//       .post(path)
-//       .send(req);
-//   } catch (e) {
-//     expect(e.message).toEqual('good');
-//   }
-//
-//   if (res.statusCode) {
-//     expect(response.statusCode).toBe(res.statusCode);
-//   } else {
-//     expect(response.statusCode).toBeGreaterThanOrEqual(200);
-//     expect(response.statusCode).toBeLessThan(300);
-//   }
-//
-//   if (res.body) {
-//     expect(response.body).toBe(res.body);
-//   }
-// }
+const mg = {
+  email: 'jestemail@email.com',
+  password: 'pass',
+  company_name: 'jestcompany',
+  name: 'name',
+};
 
-describe('POST /notebook/:notebook_hash', () => {
-  it('should return a user hash', async () => {
-    // const req = {
-    //   email: 'testuser1@email.com',
-    //   password: 'testpassword1',
-    //   company_name: 'test',
-    // };
-    //
-    // await testApi('/register', req);
+beforeAll(async () => {
+  await firebaseUtil.deleteCompany(mg.company_name);
+
+  try {
+    const path = '/register';
+    const req = mg;
+    const response = await request(api).post(path).send(req);
+    mg.user_hash = response.user_hash;
+  } catch (e) {
+  }
+
+  try {
+    const path = '/user';
+    const req = mg;
+    const response = await request(api).post(path).send(req);
+    mg.user = response;
+  } catch (e) {
+  }
+
+  try {
+    const path = '/addNotebook';
+    const req = mg;
+    const response = await request(api).post(path).send(req);
+    mg.notebook = response;
+  } catch (e) {
+  }
+});
+
+describe('GET /notebook/:notebook_hash', () => {
+  it('Get a fail not being a user', async () => {
+    let response;
+    try {
+      const path = `/notebook/${mg.notebook.notebook_hash}`;
+      const req = mg;
+      response = await request(api).post(path).send(req);
+    } catch (e) {
+      expect(e.message).toEqual('good');
+    }
+
+    expect(response.statusCode).toBe(404);
   });
 });
